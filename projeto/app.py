35c0fdb4bad530aa8955833325cfd832d03be9c1
luis <a108648@alunos.uminho.pt>
2026-04-28 15:01:47 +0100

    v1.0

--- projeto/app.py
+++ projeto/app.py
@@ -807,4 +807,4 @@ def check():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
